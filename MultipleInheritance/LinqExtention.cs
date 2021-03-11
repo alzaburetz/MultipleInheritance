5e827a52060355155668ece1a5d458993ea8bf73
anton.motin.dev@gmail.com <anton.motin.dev@gmail.com>
2021-03-12 01:46:12 +0300

    Custom enumerator

--- MultipleInheritance/LinqExtention.cs
+++ MultipleInheritance/LinqExtention.cs
@@ -1,41 +1,97 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Text;
 
 namespace System.Linq
 {
+    public class MyEnumerator<T> : IEnumerator<T>, IEnumerable<T>
+    {
+        public List<T> Data;
+        private int index = -1;
+        public T Current
+        {
+            get
+            {
+                if (index == -1)
+                    MoveNext();
+                return Data[index];
+            }
+        }
+
+        object IEnumerator.Current => throw new NotImplementedException();
+
+        public void Dispose()
+        {
+            
+        }
+
+        public bool MoveNext()
+        {
+            int position = this.index + 1;
+
+            if (position >= 0 && position < this.Data.Count)
+            {
+                this.index = position;
+                return true;
+            }
+
+            else
+            {
+                return false;
+            }
+        }
+
+        public void Reset()
+        {
+            this.index = 0;
+        }
+
+        public IEnumerator<T> GetEnumerator() => Data.GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator() => Data.GetEnumerator();
+
+        public MyEnumerator() { Data = new List<T>(); }
+
+        public MyEnumerator(List<T> data)
+        {
+            Data = new List<T>();
+            Data.AddRange(data);
+            this.index = 0;
+        }
+    }
     public static class LinqExtention
     {
-        public static IEnumerable<T> MyTake<T>(this IEnumerable<T> collection, int count)
+        public static MyEnumerator<T> MyTake<T>(this MyEnumerator<T> collection, int count)
         {
-            if (collection.Count() < count)
+            if (collection.Data.Count < count)
             {
                 throw new IndexOutOfRangeException();
             }
             var result = new List<T>();
             int i = 0;
-            foreach(var item in collection)
+            while(i < count)
             {
-                if (i < count)
+                result.Add(collection.Current);
+                if (collection.MoveNext())
                 {
-                    result.Add(item);
+                    i++;
                 }
-                i++;
             }
-            return result;
+            return new MyEnumerator<T>(result);
         }
 
-        public static IEnumerable<T> MyWhere<T>(this IEnumerable<T> collection, Predicate<T> condition)
+        public static MyEnumerator<T> MyWhere<T>(this MyEnumerator<T> collection, Predicate<T> condition)
         {
             var result = new List<T>();
-            foreach(var item in collection)
+            while (collection.MoveNext())
             {
-                if (condition.Invoke(item))
+                if (condition.Invoke(collection.Current))
                 {
-                    result.Add(item);
+                    result.Add(collection.Current);
                 }
             }
-            return result;
+            return new MyEnumerator<T>(result);
         }
     }
 }
--- MultipleInheritance/Program.cs
+++ MultipleInheritance/Program.cs
@@ -1,12 +1,28 @@
 ﻿using System;
+using System.Linq;
 
 namespace MultipleInheritance
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+
+        public static MyEnumerator<int> GetEnumeration()
         {
-            Console.WriteLine("Hello World!");
+            int i = 0;
+            var result = new MyEnumerator<int>();
+            while (i < 1000)
+            {
+                result.Data.Add(i);
+                i++;
+            }
+            return result;
+        }
+        public static void Main(string[] args)
+        {
+            foreach (var item in GetEnumeration().MyWhere(x => x % 2 == 0).MyTake(5))
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
