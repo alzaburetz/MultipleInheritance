d30bd1c8d4fbd62abd234ce0065872f4cd4359a0
alzaburetz <anton.motin.dev@gmail.com>
2021-03-16 04:12:29 +0300

    init

---
+++ MultipleInheritance/LinqExtention.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MultipleInheritance
+{
+    public static class LinqExtention
+    {
+        public static IEnumerable<dynamic> FullJoin(this IEnumerable<Person> persons, IEnumerable<Weather> weathers, Func<Weather, string> weatherPredicate, Func<Person, string> personPredicate, Func<Person, Weather, Object> filter)
+        {
+            var personsLookup = persons.ToLookup(x => x.City);
+            var weathersLookup = weathers.ToLookup(x => x.City);
+
+            var keys = new HashSet<string>(persons.Select(x => x.City));
+            keys.UnionWith(weathersLookup.Select(x => x.Key));
+
+            var join = from key in keys
+                       let xa = personsLookup[key]
+                       let xb = weathersLookup[key]
+                       select  new { first = xa, second = xb };
+
+            return join;
+        }
+    }
+}
--- MultipleInheritance/Program.cs
+++ MultipleInheritance/Program.cs
@@ -1,12 +1,49 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace MultipleInheritance
 {
+    struct Test
+    {
+        public int Flag { get; set; }
+        public int Value { get; set; }
+    }
+
+    public class Person
+    {
+        public string Name { get; set; }
+        public string City { get; set; }
+    }
+
+    public class Weather
+    {
+        public string City { get; set; }
+        public string Now { get; set; }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var persons = new List<Person>
+    {
+        new Person { Name = "Alexey", City = "Moscow" },
+        new Person { Name = "Vladimir", City = "St. Peterburg" },
+        new Person { Name = "Sergey", City = "Vladimir" },
+    };
+
+            var weathers = new List<Weather>
+    {
+        new Weather { Now = "Solar", City = "Moscow" },
+        new Weather { Now = "Rainy", City = "Tallin" },
+    };
+
+            var join = persons.FullJoin(weathers, x => x.City, y => y.City, (first, second) => new { first, second});
+
+            foreach (var j in join)
+            {
+                Console.WriteLine($"{ j.first?.Name ?? "NULL" } | { j.second?.Now ?? "NULL" }");
+            }
         }
     }
 }
