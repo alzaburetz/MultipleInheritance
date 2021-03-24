f8b3809b9490a213050fee8c26cf13e2c0656a7d
alzaburetz <anton.motin.dev@gmail.com>
2021-03-24 21:51:52 +0300

    Exception on changing collection

--- MultipleInheritance/MyQueue.cs
+++ MultipleInheritance/MyQueue.cs
@@ -13,37 +13,32 @@ namespace MultipleInheritance
     }
     public class MyQueue<T> : IQueue<T>
     {
-        T[] data;
-        int index;
-        int version;
-        int enumeration_verion;
-        bool isReadOnly;
-        public int Count => data.Length;
+        public int Count => _data.Length;
 
-        public T Current => data[index];
+        public T Current => _data[_index];
 
-        object IEnumerator.Current => data[index];
+        object IEnumerator.Current => _data[_index];
 
         public T Dequeue()
         {
-            if (index == 0)
+            if (_index == 0)
             {
                 throw new InvalidOperationException();
             }
 
-            if (this.isReadOnly && version == enumeration_verion)
+            if (_version == _enumeration_verion)
             {
                 throw new InvalidOperationException();
             }
-            var dequeue = data[0];
+            var dequeue = _data[0];
 
-            var temp = new T[--index];
-            for (int i = 1; i < data.Length; i++)
+            var temp = new T[--_index];
+            for (int i = 1; i < _data.Length; i++)
             {
-                temp[i - 1] = data[i];
+                temp[i - 1] = _data[i];
             }
-            data = temp;
-            version++;
+            _data = temp;
+            _version++;
 
             return dequeue;
         }
@@ -59,44 +54,49 @@ namespace MultipleInheritance
                 throw new InvalidOperationException();
             } 
             
-            if (this.isReadOnly && version == enumeration_verion)
+            if (_version == _enumeration_verion)
             {
                 throw new InvalidOperationException();
             }
-            var temp = new T[++index];
-            if (this.data != null)
-                this.data.CopyTo(temp, 0);
-            this.data = temp;
-            this.data[index - 1] = data;
-            version++;
+            var temp = new T[++_index];
+            if (this._data != null)
+                this._data.CopyTo(temp, 0);
+            this._data = temp;
+            this._data[_index - 1] = data;
+            _version++;
         }
 
         public IEnumerator<T> GetEnumerator()
         {
-            enumeration_verion = version;
-            isReadOnly = true;
-            return ((IReadOnlyCollection<T>)this.data).GetEnumerator();
+            _enumeration_verion = _version;
+            return ((IReadOnlyCollection<T>)this._data).GetEnumerator();
         }
 
         public bool MoveNext()
         {
-            return ++index < data.Length;
+            return ++_index < _data.Length;
         }
 
         public void Reset()
         {
-            index = -1;
-            version = 0;
+            _index = -1;
+            _version = 0;
         }
 
         IEnumerator IEnumerable.GetEnumerator()
         {
-            return this.data.GetEnumerator();
+            return this._data.GetEnumerator();
         }
 
         public MyQueue()
         {
-            this.data = new T[0];
+            this._data = new T[0];
         }
+
+
+        T[] _data;
+        int _index;
+        int _version;
+        int _enumeration_verion = -1;
     }
 }
